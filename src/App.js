dd3558aa2b43cea0472564db30f8a7d49e224ec0
SarraISMAIL <sarraismail20@gmail.com>
2022-04-18 14:35:59 +0100

    Project structure & Auth module_front

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import AuthIndex from "./screens/AuthIndex"
+
+
+
+
+
+
+export default class App extends Component {
+  render() {
+    return (
+     
+      <div>   <AuthIndex/> </div>
+    )
+  }
+}
